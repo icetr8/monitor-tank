5803534adaba9402f48e1dd94d8515fd564be14b
angelo suinan <suinanangelo@gmail.com>
2017-07-24 18:56:53 +0800

    fix pull request error

--- src/config/settings.py
+++ src/config/settings.py
@@ -150,14 +150,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-import dj_database_url
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
