77e1bf4022a6fba370f0688cef9672e3570701ff
angelo suinan <suinanangelo@gmail.com>
2017-07-25 23:43:43 +0800

    add charts, suscribers to page

--- src/home/views.py
+++ src/home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
+from v1.models import Subscriber, Report
 
 
 class Index(View):
     template_name = 'home/index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        context = {}
+        subs = Subscriber.objects.all()
+        reports = reversed(Report.objects.all().order_by('-id')[:10])
+        context['subscriber'] = subs
+        context['reports'] = reports
+        return render(request, self.template_name, context)
